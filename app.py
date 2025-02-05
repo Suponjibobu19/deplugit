92caa8ed81c7990a49abef04b6625a3b1cb382d6
Suponjibobu19 <thibault.goutorbe@aivancity.education>
2025-02-05 11:55:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import mean_squeard_error
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 from sklearn.dataset import load_iris
 
